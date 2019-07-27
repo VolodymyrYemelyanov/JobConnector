e17abb3ad8d1acab1d1d7942cea6d23b3c62e900
VladYem <volodymyem@gmail.com>
2019-07-27 22:34:00 +0200

    User Validation

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+// Init Middleware for the body parser
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
